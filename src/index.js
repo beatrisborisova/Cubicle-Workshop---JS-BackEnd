103b48c595f51270e20478bfbf6d167559ce42cf
Beatris <beti16b@gmail.com>
2022-01-22 12:25:55 +0200

    Add layout

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const handlebars = require('express-handlebars');
+const path = require('path');
+
 const app = express();
 
+//app.set('views', path.resolve(__dirname + '/views'));
+const hbs = handlebars.create({ extname: '.hbs' })
+
+app.set('views', path.resolve('./src/views'))
+
+app.engine('.hbs', hbs.engine);
+app.set('view engine', '.hbs');
+
 app.all('/', (req, res) => {
-    res.write('It\'s working!');
-    res.end();
+    res.render('index')
 });
 app.listen(3000, console.log.bind(console, 'Application is running on http://localhost:3000')); // тука console.log-а може и с arrow funcition
